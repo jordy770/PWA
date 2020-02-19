8f516fee29f30400fcc4b3264d4c7b06374e9259
jordy770 <jordy.van.santen@hotmail.com>
2020-02-19 21:02:25 +0100

    Implement localforage

--- app.js
+++ app.js
@@ -1,162 +1,46 @@
-// const main = document.querySelector('main');
-
-// window.addEventListener('load', e => {
-//     updateProjects();
-
-//     if ('serviceWorker' in navigator) {
-//         try {
-//             navigator.serviceWorker.register('sw.js');
-//             console.log('SW Registered')
-//         } catch (error) {
-//             console.log('SW reg failed')
-
-//         }
-//     }
-// });
-
-// async function updateProjects() {
-//     const res = await fetch('https://cmgt.hr.nl:8000/api/projects/');
-//     const json = await res.json();
-
-//     main.innerHTML = json.projects.map(createProject).join('\n');
-// }
-
-
-
-// const dbName = this.result;
-
-// const request = indexedDB.open("projectsDB", 1);
-
-// request.onerror = function(event){
-//   console.log(event);
-// }
-
-// request.onsuccess = function(event){
-//   console.log("success");
-// }
-
-// request.onupgradeneeded = function(event){
-//   const db = event.target.result;
-//   const objectStore = db.createObjectStore("projects", { keyPath: "id", autoIcrement: true });
-
-//   objectStore.createIndex("title", "title", { unique: false });
-// }
-
 if (!window.indexedDB) {
-    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+  window.alert("Your browser doesn't support a stable version of IndexedDB.");
 }
 
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 
-window.addEventListener('load', e => {
-    updateProjects();
+window.addEventListener("load", e => {
+  updateProjects();
 
-    if ('serviceWorker' in navigator) {
-        try {
-            navigator.serviceWorker.register('sw.js');
-            console.log('SW Registered')
-        } catch (error) {
-            console.log('SW reg failed')
-
-        }
+  if ("serviceWorker" in navigator) {
+    try {
+      navigator.serviceWorker.register("sw.js");
+      console.log("SW Registered");
+    } catch (error) {
+      console.log("SW reg failed");
     }
+  }
 });
 
 async function updateProjects() {
-    const rawData = await fetch('https://cmgt.hr.nl:8000/api/projects/');
-    const jsonData = await rawData.json();
-    console.log(jsonData.projects);
-    main.innerHTML = jsonData.projects.map(createProject).join('\n');
-    var db = window.indexedDB.open("projectsDB", 1);
-    db.onerror = function(event){
-        console.log('error');
-    }
-    db.oncomplete = function(){
-        console.log('complete');
-    }
-    db.onsuccess = function(){
-        console.log('succes');
-        console.log(db);
-    }
-    db.onupgradeneeded = function(event){
-        console.log('upgrade!');
-        console.log(event.target);
-        let objectStore = db.createObjectStore("project", { keyPath: "id" });
+  const rawData = await fetch("https://cmgt.hr.nl:8000/api/projects/");
+  const jsonData = await rawData.json();
+  //   console.log(jsonData.projects);
+  main.innerHTML = jsonData.projects.map(createProject).join("\n");
 
-            for (let i in jsonData) {
-                objectStore.add(jsonData.projects[i]);
-            }
-    }
-    
-        // .onerror(function(event){
-        //     console.log("error: ");
-        // })
-        // .onsuccess(e, (event) => {
-        //     db = request.result;
-        //     console.log("success: " + db);
-        // })  
-        // .onupgradeneeded(e, (event) => {
-        //     var db = event.target.result;
-        //     var objectStore = db.createObjectStore("project", { keyPath: "id" });
+  var projectIds1 = [];
 
-        //     for (let i in jsonData) {
-        //         objectStore.add(jsonData.projects[i]);
-        //     }
-        // }
-        // ).oncomplete(e, (event) => {
-        //     console.log(e);
-        //     console.log(event);
-        // });
-
-    // transaction = db.transaction(["project"], "readwrite")
-    //     .oncomplete(e, (event) => {
-    //         console.log("data added");
-    //     })
-    //     .onerror(e, (event) => {
-    //         console.log(" erooorrrrrrrrrrr")
-    //     })
-    //     console.log(projectsDB);
+  jsonData.projects.forEach(function(project) {
+    projectIds1.push(project._id);
+    localforage
+      .setItem(project._id, project)
+    //   .then(function(value) {
+    //     console.log(value);
+    //   })
+      .catch(function(err) {
+        console.error(err);
+      });
+  });
 
 }
 
-// var db;
-// var request = window.indexedDB.open("projectsDB", 1);
-
-// request.onerror = function (event) {
-//     console.log("error: ");
-// };
-// request.onsuccess = function (event) {
-//     db = request.result;
-//     console.log("success: " + db);
-// };
-
-// request.onupgradeneeded = function (event) {
-//     var db = event.target.result;
-//     var objectStore = db.createObjectStore("project", { keyPath: "id" });
-
-//     for (var i in jsonData) {
-//         objectStore.add(jsonData[i]);
-//     }
-// }
-
-// function add(){
-//     const rawData = await fetch('https://cmgt.hr.nl:8000/api/projects/');
-//     const jsonData = await rawData.json();
-//     var request = db.transaction(["projectsDB"], "readwrite")
-//     .objectStore("projectsDB")
-//     .add(jsonData);
-    
-//     request.onsuccess = function(event) {
-//        alert("Kenny has been added to your database.");
-//     };
-    
-//     request.onerror = function(event) {
-//        alert("Unable to add data\r\nKenny is aready exist in your database! ");
-//     }
-// }
-
-function createProject(project) {
-    return `
+function createProject(project) { 
+  return `
         <div class="project">
             <h2>${project.title}</h2>
             <img src="https://cmgt.hr.nl:8000/${project.headerImage}" alt="${project.title}"></img>
@@ -164,3 +48,15 @@ function createProject(project) {
         </div>
     `;
 }
+
+localforage.length().then(function(numberOfKeys) {
+    // Outputs the length of the database.
+    for (let i = 0; i >= numberOfKeys; i++){
+        console.log(numberOfKeys);
+    }
+
+}).catch(function(err) {
+    // This code runs if there were any errors
+    console.log(err);
+});
+
